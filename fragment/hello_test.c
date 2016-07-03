77b236503e30e324c06e2ced57e74a9a55a0b0d9
rqsong_outlook <rqsong@outlook.com>
2016-07-03 17:33:48 +0800

    add the documents named fragment & hello_print

---
+++ fragment/hello_test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+//print the classic sentence "hello world" for test the git and gcc tools.
+
+int main(int argc, char* argv[])
+{
+	int loop = 0;
+
+	for(;loop < 10;loop++)
+	{
+	 printf("\n\thello world\n");
+	}
+	
+	return 0;
+}
+
+
